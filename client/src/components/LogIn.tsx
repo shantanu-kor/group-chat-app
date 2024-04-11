7a051ff95e637e8ee0b7d4184b9e14c1d0d8ba00
Shantanu Nitin Kor <kor.shantanu1@gmail.com>
2024-04-11 13:28:52 +0530

    Added login

--- client/src/components/LogIn.tsx
+++ client/src/components/LogIn.tsx
@@ -1,30 +1,22 @@
 import axios from "axios";
 import React, { useRef } from "react";
 
-const Signup = (props: any) => {
-  const nameRef = useRef<HTMLInputElement>(null);
+const LogIn = (props: any) => {
   const emailRef = useRef<HTMLInputElement>(null);
-  const phoneRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const name = nameRef.current?.value;
     const email = emailRef.current?.value;
-    const phone = phoneRef.current?.value;
     const password = passwordRef.current?.value;
     let data;
     try {
-      data = await axios.post("http://localhost:3000/user/sign-up", {
-        name,
+      data = await axios.post("http://localhost:3000/user/log-in", {
         email,
-        phone,
         password,
       });
       data = data.data.message;
-      (nameRef.current as { value: string }).value = "";
       (emailRef.current as { value: string }).value = "";
-      (phoneRef.current as { value: string }).value = "";
       (passwordRef.current as { value: string }).value = "";
     } catch (err) {
       data = (err as { response: { data: { message: string } } }).response.data
@@ -36,35 +28,17 @@ const Signup = (props: any) => {
   return (
     <React.Fragment>
       <div className="border border-black 2xl:mx-[34%] xl:mx-[30%] lg:mx-[25%] sm:mx-[19%] mx-[15%] rounded-xl bg-violet-800 text-white">
-        <h2 className="md:text-3xl text-2xl text-center m-2">Signup</h2>
+        <h2 className="md:text-3xl text-2xl text-center m-2">Login</h2>
         <form
           className="md:text-2xl text-1xl text-center"
           onSubmit={submitHandler}
         >
-          <label htmlFor="name">Name: </label>
-          <input
-            id="name"
-            type="text"
-            className="border border-black my-2 text-black bg-violet-300"
-            ref={nameRef}
-            required
-          />
-          <br />
           <label htmlFor="email">Email: </label>
           <input
             id="email"
             type="email"
             className="border border-black my-2 text-black bg-violet-300"
             ref={emailRef}
-            required
-          />
-          <br />
-          <label htmlFor="phone">Phone: </label>
-          <input
-            id="phone"
-            type="tel"
-            className="border border-black my-2 text-black bg-violet-300"
-            ref={phoneRef}
             required
           />
           <br />
@@ -87,11 +61,11 @@ const Signup = (props: any) => {
       </div>
       <div className="md:text-2xl text-1xl text-center my-5">
         <button className="border border-indigo-700 p-2 rounded-xl bg-indigo-700 text-white hover:bg-indigo-900" onClick={props.change}>
-          Already a User? Login
+          New User? Signup
         </button>
       </div>
     </React.Fragment>
   );
 };
 
-export default Signup;
+export default LogIn;
--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Signup from '../components/Signup';
+import LogIn from '../components/LogIn';
 
 const Auth = () => {
+  const [newUser, setNewUser] = useState(true);
+
+  const setNewUserFalse = () => {
+    setNewUser(false);
+  }
+
+  const setNewUserTrue = () => {
+    setNewUser(true);
+  }
+
   return (
     <React.Fragment>
-      <Signup />
+      {newUser ? <Signup change={setNewUserFalse} /> : <LogIn change={setNewUserTrue} />}
     </React.Fragment>
   )
 }
